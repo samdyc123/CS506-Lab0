3fca206c1e6abcaa60399d6024b4e1f521ae754c
samdyc123 <123403524+samdyc123@users.noreply.github.com>
2024-12-09 22:20:05 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,13 @@
-## Please fill in all the parts labeled as ### YOUR CODE HERE
-
 import numpy as np
 
 def dot_product(v1, v2):
     '''
     v1 and v2 are vectors of same shape.
-    return the scalar dor product of the two vectors.
+    return the scalar dot product of the two vectors.
     # Hint: use `np.dot`.
     '''
-    ### YOUR CODE HERE
-    
+    return np.dot(v1, v2)
+
 def cosine_similarity(v1, v2):
     '''
     v1 and v2 are vectors of same shape.
@@ -26,8 +24,12 @@ def cosine_similarity(v1, v2):
     
     # Hint: Use `dot_product` and `np.linalg.norm`.
     '''
-    ### YOUR CODE HERE
-    
+    norm_v1 = np.linalg.norm(v1)
+    norm_v2 = np.linalg.norm(v2)
+    if norm_v1 == 0 or norm_v2 == 0:  # Handle the case where vectors have zero magnitude
+        return 0
+    return dot_product(v1, v2) / (norm_v1 * norm_v2)
+
 def nearest_neighbor(target_vector, vectors):
     '''
     target_vector is a vector of shape d.
@@ -38,4 +40,13 @@ def nearest_neighbor(target_vector, vectors):
     # Hint: You should use the cosine_similarity function that you already wrote.
     # Hint: For this lab, you can just use a for loop to iterate through vectors.
     '''
-    ### YOUR CODE HERE
+    best_index = -1
+    best_similarity = -1  # Cosine similarity ranges from -1 to 1
+    
+    for i, vector in enumerate(vectors):
+        similarity = cosine_similarity(target_vector, vector)
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_index = i
+    
+    return best_index
